f30f46dba36f50a6c2dc28bc03900adfee3d93ae
kevko <kevin.koch@unisg.ch>
2022-02-18 11:28:38 -0500

    removed libraries

--- vision/shared/Visualizer.cpp
+++ vision/shared/Visualizer.cpp
@@ -1,10 +1,7 @@
 #include "Visualizer.h"
 #include "AffectivaLogo.h"
 
-#include <iostream>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
 #include <iomanip>
 
 Visualizer::Visualizer():
